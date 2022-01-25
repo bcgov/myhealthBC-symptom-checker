497a105a9a1e951d44247aee7bec4ba1295b43cf
Sunghwan Park <sunghwan@freshworks.io>
2022-01-25 15:39:42 -0800

    happ-440 | Severity of difficult breathing

--- apps/web/src/forms/SymptomChecker.tsx
+++ apps/web/src/forms/SymptomChecker.tsx
@@ -13,6 +13,8 @@ import { Recommendation } from '../types/Recommendation';
 import { Q3SymptomBreathingSeverity } from './Q3SymptomBreathingSeverity';
 import { Q3SymptomCoughSeverity } from './Q3SymptomCoughSeverity';
 import { initialValues } from '../types/initialValues';
+import { Q3SymptomBodyAchesSeverity } from './Q3SymptomBodyAchesSeverity';
+import { Q3SymptomSoreThroatSeverity } from './Q3SymptomSoreThroatSeverity';
 
 export const SymptomChecker = () => {
   const { t } = useTranslation();
@@ -67,6 +69,8 @@ export const SymptomChecker = () => {
     <Q3Symptoms key={2} values={values} onChange={onChange} />,
     <Q3SymptomBreathingSeverity key={3} values={values} onChange={onChange} />,
     <Q3SymptomCoughSeverity key={4} values={values} onChange={onChange} />,
+    <Q3SymptomBodyAchesSeverity key={5} values={values} onChange={onChange} />,
+    <Q3SymptomSoreThroatSeverity key={6} values={values} onChange={onChange} />,
     <Q4TestResult key={100} values={values} onChange={onChange} />,
   ];
 
@@ -102,7 +106,6 @@ export const SymptomChecker = () => {
           </Button>
           <span className='ml-4'>
             <Button
-              title={t('Continue')}
               type={pages.length - 1 === page ? 'button' : 'submit'}
               variant='primary'
               widthClass='md:w-44'
@@ -112,12 +115,12 @@ export const SymptomChecker = () => {
             </Button>
           </span>
         </div>
-        {/* <div className='text-sm bg-slate-100 p-4 max-h-56 overflow-auto'>
+        <div className='text-sm bg-slate-100 p-4 max-h-56 overflow-auto'>
           <pre>
             <strong>{'values => '}</strong>
             {JSON.stringify(values, null, 2)}
           </pre>
-        </div> */}
+        </div>
       </FormikProvider>
     </div>
   );
