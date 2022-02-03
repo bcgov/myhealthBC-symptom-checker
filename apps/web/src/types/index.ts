0f1ba15d4dedc0cdd0324f0ef85abc70d2797509
Sunghwan Park <sunghwan@freshworks.io>
2022-02-03 09:44:22 -0800

    fix error message of symptom selection page

--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -97,12 +97,10 @@ export const initialValues: SymptomCheckerForm = {
 };
 
 export const validationSchema = {
-  symptoms: yup.object().shape({
-    symptoms: yup.object().test('required', (value, context) => {
-      const isSet = Object.values(value).some((s: SymptomDetails) => s.checked);
-      if (isSet) return true;
-      throw new yup.ValidationError('Required', value, context.path);
-    }),
+  symptoms: yup.object().test('required', (value, context) => {
+    const isSet = Object.values(value).some((s: SymptomDetails) => s.checked);
+    if (isSet) return true;
+    throw new yup.ValidationError('Required', value, context.path);
   }),
   test: yup.object().shape({
     test: yup.object().shape({
