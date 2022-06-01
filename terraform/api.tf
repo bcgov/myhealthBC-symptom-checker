c8ee5a87b2ab9321376d78f22346c4b48403bfca
David Bayly <dbayly@freshworks.io>
2022-06-01 13:00:58 -0700

    Removed resources for db and api, added default cloudfront certificate for production

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,98 +1,98 @@
-resource "aws_lambda_function" "api" {
-  description      = "Symptom Checker API for ${local.namespace}"
-  function_name    = local.api_name
-  role             = aws_iam_role.lambda.arn
-  runtime          = "nodejs14.x"
-  filename         = local.api_build_zip
-  source_code_hash = filebase64sha256(local.api_build_zip)
-  handler          = "lambda.handler"
-  memory_size      = 512
-  timeout          = 30
+# resource "aws_lambda_function" "api" {
+#   description      = "Symptom Checker API for ${local.namespace}"
+#   function_name    = local.api_name
+#   role             = aws_iam_role.lambda.arn
+#   runtime          = "nodejs14.x"
+#   filename         = local.api_build_zip
+#   source_code_hash = filebase64sha256(local.api_build_zip)
+#   handler          = "lambda.handler"
+#   memory_size      = 512
+#   timeout          = 30
 
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      filename,
-      source_code_hash,
-      source_code_size,
-      last_modified,
-    ]
-  }
+#   lifecycle {
+#     ignore_changes = [
+#       # Ignore changes to tags, e.g. because a management agent
+#       # updates these based on some ruleset managed elsewhere.
+#       filename,
+#       source_code_hash,
+#       source_code_size,
+#       last_modified,
+#     ]
+#   }
 
-  vpc_config {
-    security_group_ids = [module.network.aws_security_groups.app.id]
-    subnet_ids         = module.network.aws_subnet_ids.app.ids
-  }
+#   vpc_config {
+#     security_group_ids = [module.network.aws_security_groups.app.id]
+#     subnet_ids         = module.network.aws_subnet_ids.app.ids
+#   }
 
-  environment {
-    variables = {
-      TARGET_ENV  = var.target_env
-      RUNTIME_ENV = "hosted"
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       TARGET_ENV  = var.target_env
+#       RUNTIME_ENV = "hosted"
+#     }
+#   }
+# }
 
-resource "aws_apigatewayv2_api" "api" {
-  name          = local.api_name
-  protocol_type = "HTTP"
-}
+# resource "aws_apigatewayv2_api" "api" {
+#   name          = local.api_name
+#   protocol_type = "HTTP"
+# }
 
-resource "aws_apigatewayv2_integration" "api" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  connection_type    = "INTERNET"
-  description        = local.api_name
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.api.invoke_arn
-}
+# resource "aws_apigatewayv2_integration" "api" {
+#   api_id             = aws_apigatewayv2_api.api.id
+#   integration_type   = "AWS_PROXY"
+#   connection_type    = "INTERNET"
+#   description        = local.api_name
+#   integration_method = "POST"
+#   integration_uri    = aws_lambda_function.api.invoke_arn
+# }
 
-resource "aws_apigatewayv2_route" "api" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.api.id}"
-}
+# resource "aws_apigatewayv2_route" "api" {
+#   api_id    = aws_apigatewayv2_api.api.id
+#   route_key = "$default"
+#   target    = "integrations/${aws_apigatewayv2_integration.api.id}"
+# }
 
-locals {
-  api_gateway_log_format_with_newlines = <<EOF
-{ 
-"requestId":"$context.requestId",
-"ip":"$context.identity.sourceIp",
-"requestTime":"$context.requestTime",
-"httpMethod":"$context.httpMethod",
-"status":"$context.status",
-"path":"$context.path",
-"responseLength":"$context.responseLength",
-"errorMessage":"$context.error.message"
-}
-EOF
-  api_gateway_log_format               = replace(local.api_gateway_log_format_with_newlines, "\n", "")
-}
+# locals {
+#   api_gateway_log_format_with_newlines = <<EOF
+# { 
+# "requestId":"$context.requestId",
+# "ip":"$context.identity.sourceIp",
+# "requestTime":"$context.requestTime",
+# "httpMethod":"$context.httpMethod",
+# "status":"$context.status",
+# "path":"$context.path",
+# "responseLength":"$context.responseLength",
+# "errorMessage":"$context.error.message"
+# }
+# EOF
+#   api_gateway_log_format               = replace(local.api_gateway_log_format_with_newlines, "\n", "")
+# }
 
-resource "aws_cloudwatch_log_group" "api_gateway" {
-  name = "api-gw/${local.api_name}/logs"
+# resource "aws_cloudwatch_log_group" "api_gateway" {
+#   name = "api-gw/${local.api_name}/logs"
 
-  lifecycle {
-    ignore_changes = [
-      retention_in_days
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       retention_in_days
+#     ]
+#   }
+# }
 
-resource "aws_apigatewayv2_stage" "api" {
-  api_id      = aws_apigatewayv2_api.api.id
-  name        = "$default"
-  auto_deploy = true
+# resource "aws_apigatewayv2_stage" "api" {
+#   api_id      = aws_apigatewayv2_api.api.id
+#   name        = "$default"
+#   auto_deploy = true
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gateway.arn
-    format          = local.api_gateway_log_format
-  }
-}
+#   access_log_settings {
+#     destination_arn = aws_cloudwatch_log_group.api_gateway.arn
+#     format          = local.api_gateway_log_format
+#   }
+# }
 
-resource "aws_lambda_permission" "api_allow_gateway" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.api.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_stage.api.execution_arn}/*"
-}
+# resource "aws_lambda_permission" "api_allow_gateway" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.api.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_apigatewayv2_stage.api.execution_arn}/*"
+# }
--- terraform/db.tf
+++ terraform/db.tf
@@ -1,38 +1,38 @@
 
-resource "aws_db_subnet_group" "pgsql" {
-  name       = "pgsql"
-  subnet_ids = module.network.aws_subnet_ids.data.ids
-}
+# resource "aws_db_subnet_group" "pgsql" {
+#   name       = "pgsql"
+#   subnet_ids = module.network.aws_subnet_ids.data.ids
+# }
 
-resource "aws_rds_cluster_instance" "pgsql" {
-  count              = var.target_env == "prod" ? 2 : 1
-  identifier         = "${local.db_name}-${count.index}"
-  cluster_identifier = aws_rds_cluster.pgsql.id
-  instance_class     = "db.r5.large" # 1 core 2vCPU 16 GiB Memory
-  engine             = aws_rds_cluster.pgsql.engine
-  engine_version     = aws_rds_cluster.pgsql.engine_version
-}
+# resource "aws_rds_cluster_instance" "pgsql" {
+#   count              = var.target_env == "prod" ? 2 : 1
+#   identifier         = "${local.db_name}-${count.index}"
+#   cluster_identifier = aws_rds_cluster.pgsql.id
+#   instance_class     = "db.r5.large" # 1 core 2vCPU 16 GiB Memory
+#   engine             = aws_rds_cluster.pgsql.engine
+#   engine_version     = aws_rds_cluster.pgsql.engine_version
+# }
 
-resource "aws_rds_cluster" "pgsql" {
-  cluster_identifier  = local.db_name
-  engine              = "aurora-postgresql"
-  availability_zones  = ["ca-central-1a", "ca-central-1b"]
-  database_name       = replace(var.project_code, "-", "_")
-  master_username     = "symchk"
-  master_password     = data.aws_ssm_parameter.postgres_password.value
-  storage_encrypted   = true
-  deletion_protection = true
+# resource "aws_rds_cluster" "pgsql" {
+#   cluster_identifier  = local.db_name
+#   engine              = "aurora-postgresql"
+#   availability_zones  = ["ca-central-1a", "ca-central-1b"]
+#   database_name       = replace(var.project_code, "-", "_")
+#   master_username     = "symchk"
+#   master_password     = data.aws_ssm_parameter.postgres_password.value
+#   storage_encrypted   = true
+#   deletion_protection = true
 
-  db_subnet_group_name   = aws_db_subnet_group.pgsql.name
-  vpc_security_group_ids = [module.network.aws_security_groups.data.id]
+#   db_subnet_group_name   = aws_db_subnet_group.pgsql.name
+#   vpc_security_group_ids = [module.network.aws_security_groups.data.id]
 
-  # 2AM-4AM PST
-  preferred_backup_window = "09:00-11:00"
-  backup_retention_period = var.target_env == "prod" ? 14 : 3
+#   # 2AM-4AM PST
+#   preferred_backup_window = "09:00-11:00"
+#   backup_retention_period = var.target_env == "prod" ? 14 : 3
 
-  lifecycle {
-    ignore_changes = [
-      availability_zones
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       availability_zones
+#     ]
+#   }
+# }
