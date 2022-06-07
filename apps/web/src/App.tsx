ac6e559eda4dcb58097a6910e481fa7e396a793a
David Bayly <54554766+dbayly-freshworks@users.noreply.github.com>
2022-06-07 09:18:00 -0700

    Adding snowplow script to test (#66)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,19 +6,21 @@ import { SymptomChecker } from './forms/SymptomChecker';
 import { ResultPage } from './results/ResultPage';
 const App = () => {
   useEffect(() => {
-    if (process.env.REACT_APP_ENV_NAME === 'dev') {
-      console.log('Adding Snowplow script');
-      const script = document.createElement('script');
-      script.src = '/analytics/snowplow.dev.js';
-      script.async = true;
-
-      document.body.appendChild(script);
-      return () => {
-        document.body.removeChild(script);
-      };
-    } else {
-      console.log('Snowplow not enabled for this env');
+    let script = document.createElement('script');
+    switch (process.env.REACT_APP_ENV_NAME) {
+      case 'dev':
+      case 'test':
+        console.log('Adding Snowplow script');
+        script.src = '/analytics/snowplow.dev.js';
+        script.async = true;
+        break;
+      default:
+        return;
     }
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
   return (
     <BrowserRouter>
