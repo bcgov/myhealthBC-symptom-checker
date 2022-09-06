ee8e7f0ec61e3816fbc1a586ad9e930488493f88
Tara Epp <tara@freshworks.io>
2022-09-06 14:10:12 -0600

    Updated text and test criteria for rapid test result

--- apps/web/src/results/ResultRapidTest.tsx
+++ apps/web/src/results/ResultRapidTest.tsx
@@ -19,14 +19,42 @@ export const ResultRapidTest = () => {
     >
       <div>
         <p>{t('ResultRapidTestDescription1')}</p>
-        <p>
-          <Trans i18nKey='ResultRapidTestNotice'>
+        <Trans i18nKey='ResultRapidTestDescription2'>
+          <p>
             <Link href='https://www2.gov.bc.ca/gov/content/covid-19/info/testing#get'>
               Rapid antigen testing kits are free for everyone
             </Link>
             . Visit your local pharmacy and ask for your testing kit.
-          </Trans>
-        </p>
+          </p>
+        </Trans>
+        <Trans i18nKey='ResultRapidTestDescription3'>
+          <p>You can get a PCR COVID-19 test if you:</p>
+          <ul className='list-disc pl-6'>
+            <li>Identify as an Indigenous person (proof of ancestry is not required), or</li>
+            <li>
+              Work in health care at a hospital, long-term care facility, assisted living facility
+              or community clinic, or
+            </li>
+            <li>
+              Work as a first responder (police officer, firefighter, emergency medical technician
+              or paramedic, or
+            </li>
+            <li>
+              Live or work in a congregate setting, like a long-term care facility, shelter,
+              correctional facility or group home, or
+            </li>
+            <li>
+              Live in a community that is far from testing centres and hospitals, including rural,
+              remote, isolated or Indigenous communities or a work camp.
+            </li>
+          </ul>
+
+          <p>
+            <Link href='http://www.bccdc.ca/health-info/diseases-conditions/covid-19/testing/where-to-get-a-covid-19-test-in-bc'>
+              Find out where to get a COVID-19 test in B.C.
+            </Link>
+          </p>
+        </Trans>
       </div>
     </ResultPageLayout>
   );
